aa6fa3e64be0b8b964d35ba1648b77764104d2c0
Ivan Kochurkin <kvanttt@gmail.com>
2021-02-11 22:07:14 +0300

    Remove Example6.java file, extend Misc tests

--- src/test/kotlin/ExcessNullCheckerTest.kt
+++ src/test/kotlin/ExcessNullCheckerTest.kt
@@ -32,11 +32,6 @@ class ExcessNullCheckerTest {
     }
 
     @Test
-    fun test6() {
-        testFile("Example6")
-    }
-
-    @Test
     fun fields() {
         testFile("Fields")
     }
--- src/test/resources/Misc.java
+++ src/test/resources/Misc.java
@@ -1,6 +1,11 @@
 package test;
 
 public class Misc {
+    static void nullEqualsNull() {
+        if (null == null) { // Test: condition_is_always_true
+        }
+    }
+
     static void doubleCheckTheSameCondition(Object x) {
         if (x == null) {
             if (x == null) { // Test: condition_is_always_true
@@ -11,6 +16,24 @@ public class Misc {
     static void unknownCondition(int x) {
         Object a = x > 7 ? new Object() : null;
         if (a == null) { // No warning
+        }
+    }
+
+    static void assignInBothBranches(int x) {
+        Object a = x > 7 ? new Object() : new Object();
+        if (a == null) { // Test: condition_is_always_false
+        }
+    }
+
+    static void useVarInDifferentBlocks(int x) {
+        Object b = new Object();
+        if (x > 7) {
+            b = null;
+            if (b == null) { // Test: condition_is_always_true
+            }
+        }
+
+        if (b == null) { // No warning
         }
     }
 
